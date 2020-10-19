d015ad14e525e7fc2ece4a1d3a06faafe040ecc6
DESKTOP-0TCN8ST\jhlsm <jhlsmart92@gmail.com>
2020-10-20 06:39:01 +0900

    rendering

---
+++ src/Movie.js
@@ -0,0 +1,31 @@
+// import React from "React";
+// import PropTypes from "prop-types";
+
+// function Movie({id, year, title, summary, poster}) {
+//     return  < h4 > {title}< /h4>;
+// }
+   
+//     Movie.PropTypes = {
+//         id: PropTypes.number.isRequired,
+//         year: PropTypes.number.isRequired,
+//         title: PropTypes.string.isRequired,
+//         summary: PropTypes.string.isRequired,
+//         poster: PropTypes.string.isRequired
+//     };
+
+// export default Movie;
+
+import React from "react";
+import PropTypes from "prop-types";
+
+function Movie({ id, year, title, summary, poster }) {
+  return <h4>{title}</h4>;
+}
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
+};
+export default Movie;
